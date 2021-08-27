0e942d0304ad37daa9f96a18e0ae076ef50926d2
eric <howcrazys@gmail.com>
2021-08-28 00:08:40 +0800

    feat:  index

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,11 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  }
 });
