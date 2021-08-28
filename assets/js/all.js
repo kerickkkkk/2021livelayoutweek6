aaf2108bdd02f0c765bb376e9b5e05b92988e14c
eric <howcrazys@gmail.com>
2021-08-28 12:04:29 +0800

    Update 2021-08-28T04:04:14.715Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,14 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
